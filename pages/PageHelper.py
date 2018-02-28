0a47401ce49d9940b159ce1cce3db1dbe120bc8a
Asad Hasen <d-asha1@Dev.Green.Net>
2018-02-28 17:09:24 +0000

    updating, wip

--- pages/PageHelper.py
+++ pages/PageHelper.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 class PageHelper:
     def __init__(self, driver):
         self.driver=driver
-        self.wait = WebDriverWait(self.driver, 8)
+        self.wait = WebDriverWait(self.driver, 5)
 
     def waitToBeClickable(self, *locator):
         self.wait.until(expected_conditions.element_to_be_clickable(locator))
