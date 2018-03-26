a183aabf0d31a481bd4f5a8b2505aaf6f7a514da
Asad Hasen <d-asha1@Dev.Green.Net>
2018-03-26 16:10:08 +0100

    added firefox headless run functionality

--- conf/driverhelper.py
+++ conf/driverhelper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 import definitions
 from definitions import CHROME_DRIVER_PATH, GECKO_DRIVER_PATH, IE_DRIVER_PATH
@@ -12,7 +13,9 @@ def set_driver(browser):
         definitions.DRIVER = webdriver.Chrome(chrome_options=chrome_options, executable_path=CHROME_DRIVER_PATH)
 
     elif browser == "firefox":
-        definitions.DRIVER = webdriver.Firefox()
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        definitions.DRIVER = webdriver.Firefox(firefox_options=firefox_options, executable_path=GECKO_DRIVER_PATH)
 
     elif browser == "edge":
         definitions.DRIVER = webdriver.Edge()
